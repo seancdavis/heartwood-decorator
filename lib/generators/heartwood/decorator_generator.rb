9ff56d76f0ba68940cc0dc8389509f20caf488bc
Sean C Davis <scdavis41@gmail.com>
2018-06-12 18:06:14 -0400

    Remove loader and focus on generator only

--- lib/generators/heartwood/decorator_generator.rb
+++ lib/generators/heartwood/decorator_generator.rb
@@ -7,19 +7,24 @@ module Heartwood
 
     source_root File.expand_path("./templates", __dir__)
 
-    argument :model, desc: 'Name of model (e.g. User)', required: true, type: 'string'
+    argument :model_name, desc: 'Name of model (e.g. User)', required: true, type: 'string'
 
     def validate_model
       begin
-        @name = @model.classify.constantize
-        @short_name = @model.underscore
+        @model_name.classify.constantize
       rescue NameError
-        raise NameError.new("Could not find model from \"#{@model}\"")
+        raise NameError.new("Could not find model from \"#{@model_name}\"")
       end
     end
 
     def render_template
-      template 'decorator.erb', "app/decorators/#{@short_name}_decorator.rb"
+      template 'decorator.erb', "app/decorators/#{@model_name.underscore}_decorator.rb"
+    end
+
+    def include_decorator
+      insert_into_file "app/models/#{@model_name.underscore}.rb",
+                       "  include #{@model_name.classify}Decorator\n",
+                       after: "ApplicationRecord\n"
     end
 
   end
--- lib/heartwood/decorator.rb
+++ lib/heartwood/decorator.rb
@@ -1,7 +1,6 @@
 require 'rails'
 
 require 'heartwood/decorator/version'
-require 'heartwood/decorator/loader'
 
 module Heartwood
   module Decorator
