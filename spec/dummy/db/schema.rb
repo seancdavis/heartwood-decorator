cf4a3368bcf124dd412d8d01b24dfc852acf55d2
Sean C Davis <scdavis41@gmail.com>
2018-06-12 21:26:39 -0400

    Add post model to dummy

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_11_234123) do
+ActiveRecord::Schema.define(version: 2018_06_13_012550) do
 
   create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
